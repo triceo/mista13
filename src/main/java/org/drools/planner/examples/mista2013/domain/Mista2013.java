34579d8f72a4e7809b6ff5772d0c154e8541a144
Lukáš Petrovický <lpetrovi@redhat.com>
2013-02-18 09:14:00 +0100

    No need for an array, since this part is no longer performance-sensitive.

--- src/main/java/org/drools/planner/examples/mista2013/domain/Mista2013.java
+++ src/main/java/org/drools/planner/examples/mista2013/domain/Mista2013.java
@@ -3,7 +3,9 @@ package org.drools.planner.examples.mista2013.domain;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.drools.planner.api.domain.solution.PlanningEntityCollectionProperty;
 import org.drools.planner.api.domain.solution.PlanningSolution;
@@ -16,18 +18,13 @@ public class Mista2013 implements Solution<BendableScore> {
 
     private final ProblemInstance problem;
     private final Collection<Allocation> allocations;
-    /**
-     * Much better for performance than HashMap.
-     */
-    private final Allocation[][] allocationsIndexed;
+    private final Map<Job, Allocation> allocationsPerJob = new HashMap<Job, Allocation>();
     private BendableScore score;
 
     public Mista2013(final ProblemInstance input) {
         this.problem = input;
         final List<Allocation> allocations = new ArrayList<Allocation>();
-        this.allocationsIndexed = new Allocation[input.getProjects().size()][];
         for (final Project p : input.getProjects()) {
-            this.allocationsIndexed[p.getId()] = new Allocation[p.getJobs().size()];
             for (final Job j : p.getJobs()) {
                 if (j.isSink() || j.isSource()) {
                     // don't create allocations from pseudo-jobs
@@ -35,14 +32,14 @@ public class Mista2013 implements Solution<BendableScore> {
                 }
                 final Allocation a = new Allocation(j);
                 allocations.add(a);
-                this.allocationsIndexed[p.getId()][j.getId()] = a;
+                this.allocationsPerJob.put(j, a);
             }
         }
         this.allocations = Collections.unmodifiableList(allocations);
     }
 
     public Allocation getAllocation(final Job job) {
-        final Allocation a = this.allocationsIndexed[job.getParentProject().getId()][job.getId()];
+        final Allocation a = this.allocationsPerJob.get(job);
         if (a == null) {
             throw new IllegalArgumentException("Job allocation not found, this is a bug in our solution code: " + job);
         }
