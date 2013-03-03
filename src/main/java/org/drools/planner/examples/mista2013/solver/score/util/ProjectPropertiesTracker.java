b577ff8be8828be76dda9ecf70a4a3e74df17d16
Lukáš Petrovický <lpetrovi@redhat.com>
2013-03-03 12:25:00 +0100

    Similar performance improvements in ProjectPropertiesTracker.

--- src/main/java/org/drools/planner/examples/mista2013/solver/score/util/ProjectPropertiesTracker.java
+++ src/main/java/org/drools/planner/examples/mista2013/solver/score/util/ProjectPropertiesTracker.java
@@ -1,5 +1,8 @@
 package org.drools.planner.examples.mista2013.solver.score.util;
 
+import gnu.trove.map.TObjectIntMap;
+import gnu.trove.map.hash.TObjectIntHashMap;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,7 +14,15 @@ import org.drools.planner.examples.mista2013.domain.Project;
 
 public class ProjectPropertiesTracker {
 
-    private static final PriorityQueue<Integer> getPriorityQueue(final int size) {
+    private static int calculateProjectDelay(final Project p, final int maxDueDate) {
+        return Math.max(0, maxDueDate + 1 - p.getReleaseDate() - p.getCriticalPathDuration());
+    }
+
+    private static int getMax(final PriorityQueue<Integer> q) {
+        return q.isEmpty() ? 0 : q.peek();
+    }
+
+    private static PriorityQueue<Integer> getPriorityQueue(final int size) {
         return new PriorityQueue<Integer>(size, Collections.reverseOrder());
     }
 
@@ -21,9 +32,10 @@ public class ProjectPropertiesTracker {
 
     private final Map<Project, PriorityQueue<Integer>> dueDatesPerProject;
 
-    private final Map<Project, Integer> projectDelays;
-
+    private final TObjectIntMap<Project> projectDelays;
     private int totalProjectDelay = 0;
+
+    private int totalMakespan;
 
     private static final int ESTIMATED_NUMBER_OF_JOBS_PER_PROJECT = 10;
 
@@ -33,65 +45,41 @@ public class ProjectPropertiesTracker {
         this.dueDates = ProjectPropertiesTracker.getPriorityQueue(projectCount
                 * ProjectPropertiesTracker.ESTIMATED_NUMBER_OF_JOBS_PER_PROJECT);
         this.dueDatesPerProject = new HashMap<Project, PriorityQueue<Integer>>(projectCount);
-        this.projectDelays = new HashMap<Project, Integer>(projectCount);
+        this.projectDelays = new TObjectIntHashMap<Project>(projectCount);
+        this.totalMakespan = this.calculateTotalMakespan(0);
     }
 
     public void add(final Allocation a) {
-        final Project currentProject = a.getJob().getParentProject();
-        final Integer dueDate = a.getDueDate();
-        PriorityQueue<Integer> perProject = this.dueDatesPerProject.get(currentProject);
+        final Project p = a.getJob().getParentProject();
+        final int newMax = a.getDueDate();
+        final Integer newMaxI = Integer.valueOf(newMax);
+        // prepare data
+        final int previousGlobalMax = ProjectPropertiesTracker.getMax(this.dueDates);
+        this.dueDates.add(newMaxI);
+        PriorityQueue<Integer> perProject = this.dueDatesPerProject.get(p);
         if (perProject == null) {
-            perProject = ProjectPropertiesTracker.getPriorityQueue(currentProject.getJobs().size());
-            this.dueDatesPerProject.put(currentProject, perProject);
+            perProject = ProjectPropertiesTracker.getPriorityQueue(p.getJobs().size());
+            this.dueDatesPerProject.put(p, perProject);
         }
-        perProject.add(dueDate);
-        this.dueDates.add(dueDate);
-        final int newDelay = this.calculateProjectDelay(currentProject);
-        final Integer previousDelay = this.projectDelays.put(currentProject, newDelay);
-        if (previousDelay == null) {
-            this.totalProjectDelay += newDelay;
-        } else {
+        final int previousLocalMax = ProjectPropertiesTracker.getMax(perProject);
+        perProject.add(newMaxI);
+        // update project properties
+        if (newMax > previousLocalMax) {
+            final int newDelay = ProjectPropertiesTracker.calculateProjectDelay(p, newMax);
+            final int previousDelay = this.projectDelays.put(p, newDelay);
             this.totalProjectDelay += newDelay - previousDelay;
+            if (newMax > previousGlobalMax) {
+                this.totalMakespan = this.calculateTotalMakespan(newMax);
+            }
         }
     }
 
-    private int calculateMakespan(final Project p) {
-        /*
-         * due date for a task is the latest time when the task is still
-         * running. due date of a project is the time after the last job
-         * finishes, hence +1.
-         */
-        return (this.getMaxDueDate(p) + 1) - p.getReleaseDate();
-    }
-
-    private int calculateProjectDelay(final Project p) {
-        return Math.max(0, this.calculateMakespan(p) - p.getCriticalPathDuration());
-    }
-
-    private int getMaxDueDate() {
-        if (this.dueDates.isEmpty()) {
-            return 0;
-        } else {
-            return this.dueDates.peek();
-        }
-    }
-
-    private int getMaxDueDate(final Project p) {
-        final PriorityQueue<Integer> perProject = this.dueDatesPerProject.get(p);
-        if (perProject == null || perProject.isEmpty()) {
-            return 0;
-        } else {
-            return perProject.peek();
-        }
+    private int calculateTotalMakespan(final int maxDueDate) {
+        return maxDueDate + 1 - this.problem.getMinReleaseDate();
     }
 
     public int getTotalMakespan() {
-        /*
-         * due date for a task is the latest time when the task is still
-         * running. due date of a project is the time after the last job
-         * finishes, hence +1.
-         */
-        return (this.getMaxDueDate() + 1) - this.problem.getMinReleaseDate();
+        return this.totalMakespan;
     }
 
     public int getTotalProjectDelay() {
@@ -99,15 +87,24 @@ public class ProjectPropertiesTracker {
     }
 
     public void remove(final Allocation a) {
-        final Project currentProject = a.getJob().getParentProject();
-        // update due dates
-        final Integer dueDate = a.getDueDate();
-        this.dueDatesPerProject.get(currentProject).remove(dueDate);
-        this.dueDates.remove(dueDate);
-        // recalculate project delay
-        final int newDelay = this.calculateProjectDelay(currentProject);
-        final int previousDelay = this.projectDelays.put(currentProject, newDelay);
-        this.totalProjectDelay += newDelay - previousDelay;
+        final Project p = a.getJob().getParentProject();
+        final int previousMax = a.getDueDate();
+        final Integer previousMaxI = Integer.valueOf(previousMax);
+        // prepare data
+        this.dueDates.remove(previousMaxI);
+        final PriorityQueue<Integer> perProject = this.dueDatesPerProject.get(p);
+        perProject.remove(previousMaxI);
+        final int newLocalMax = ProjectPropertiesTracker.getMax(perProject);
+        // update project properties
+        if (previousMax > newLocalMax) {
+            final int newDelay = ProjectPropertiesTracker.calculateProjectDelay(p, newLocalMax);
+            final int previousDelay = this.projectDelays.put(p, newDelay);
+            this.totalProjectDelay += newDelay - previousDelay;
+            final int newGlobalMax = ProjectPropertiesTracker.getMax(this.dueDates);
+            if (previousMax > newGlobalMax) {
+                this.totalMakespan = this.calculateTotalMakespan(newGlobalMax);
+            }
+        }
     }
 
 }
