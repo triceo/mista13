a5d2d9eeb86b8f326da98e8dde7539a6b0666d69
Lukáš Petrovický <lpetrovi@redhat.com>
2013-03-07 15:38:49 +0100

    Change method naming to better reflect the new nature of the tracker.

--- src/main/java/org/drools/planner/examples/mista2013/solver/score/util/PrecedenceRelationsTracker.java
+++ src/main/java/org/drools/planner/examples/mista2013/solver/score/util/PrecedenceRelationsTracker.java
@@ -17,9 +17,19 @@ public class PrecedenceRelationsTracker {
 
     private final int expectedJobCount;
 
-    private static final int ESTIMATED_NUMBER_OF_RELATIONS = 4;
+    private static final int ESTIMATED_NUMBER_OF_BONDS = 4;
 
-    private static int getBrokenRelationValidation(final Job to) {
+    private static int breakBond(final Job from, final Job to, final Map<Job, Set<Job>> relations) {
+        Set<Job> relation = relations.get(from);
+        if (relation == null) {
+            relation = new HashSet<Job>(PrecedenceRelationsTracker.ESTIMATED_NUMBER_OF_BONDS);
+            relations.put(from, relation);
+        }
+        relation.add(to);
+        return PrecedenceRelationsTracker.getBrokenPrecedenceValidation(to);
+    }
+
+    private static int getBrokenPrecedenceValidation(final Job to) {
         return 1 + to.getRecursiveSuccessors().size();
     }
 
@@ -27,19 +37,9 @@ public class PrecedenceRelationsTracker {
         return current.getStartDate() > pre.getDueDate();
     }
 
-    private static int relate(final Job from, final Job to, final Map<Job, Set<Job>> relations) {
-        Set<Job> relation = relations.get(from);
-        if (relation == null) {
-            relation = new HashSet<Job>(PrecedenceRelationsTracker.ESTIMATED_NUMBER_OF_RELATIONS);
-            relations.put(from, relation);
-        }
-        relation.add(to);
-        return PrecedenceRelationsTracker.getBrokenRelationValidation(to);
-    }
-
-    private static int unrelate(final Job from, final Job to, final Map<Job, Set<Job>> relations) {
+    private static int mendBond(final Job from, final Job to, final Map<Job, Set<Job>> relations) {
         relations.get(from).remove(to);
-        return PrecedenceRelationsTracker.getBrokenRelationValidation(to);
+        return PrecedenceRelationsTracker.getBrokenPrecedenceValidation(to);
     }
 
     private int totalCachedResult = 0;
@@ -51,94 +51,68 @@ public class PrecedenceRelationsTracker {
     private final Map<Job, Allocation> allocations;
 
     /**
-     * Relations that have been through {@link #relate(Job, Job, Map)} and not
-     * yet through {@link #unrelate(Job, Job, Map)}.
+     * Bonds that have been through {@link #breakBond(Job, Job, Map)} and not
+     * yet through {@link #mendBond(Job, Job, Map)}. This map always has a
+     * predecessor as a key and a direct successor as a value.
      */
-    private final Map<Job, Set<Job>> relations;
+    private final Map<Job, Set<Job>> brokenBonds;
 
     public PrecedenceRelationsTracker(final Mista2013 solution) {
         this.expectedJobCount = solution.getAllocations().size();
         this.allocations = new HashMap<Job, Allocation>(this.expectedJobCount);
-        this.relations = new HashMap<Job, Set<Job>>(this.expectedJobCount);
+        this.brokenBonds = new HashMap<Job, Set<Job>>(this.expectedJobCount);
     }
 
     public void add(final Allocation a) {
         final Job current = a.getJob();
         this.allocations.put(current, a);
         for (final Job j : current.getPredecessors()) {
-            this.formRelation(j, a);
+            this.bind(j, a);
         }
         for (final Job j : current.getSuccessors()) {
-            this.formRelation(a, j);
+            this.bind(a, j);
         }
         if (a.getStartDate() < current.getParentProject().getReleaseDate()) {
-            this.totalCachedResult += PrecedenceRelationsTracker.getBrokenRelationValidation(current);
+            this.totalCachedResult += PrecedenceRelationsTracker.getBrokenPrecedenceValidation(current);
         }
     }
 
-    private void breakRelation(final Allocation fromAlloc, final Allocation toAlloc) {
+    private void bind(final Allocation fromAlloc, final Allocation toAlloc) {
         final Job from = fromAlloc.getJob();
         final Job to = toAlloc.getJob();
         if (from.isSource() || to.isSink()) {
             return;
         }
-        if (this.hasRelation(from, to)) {
-            this.totalCachedResult -= PrecedenceRelationsTracker.unrelate(from, to, this.relations);
-        }
-    }
-
-    private void breakRelation(final Allocation from, final Job to) {
-        final Allocation a = this.allocations.get(to);
-        if (a == null) {
-            return;
-        }
-        this.breakRelation(from, a);
-    }
-
-    private void breakRelation(final Job from, final Allocation to) {
-        final Allocation a = this.allocations.get(from);
-        if (a == null) {
-            return;
-        }
-        this.breakRelation(a, to);
-    }
-
-    private void formRelation(final Allocation fromAlloc, final Allocation toAlloc) {
-        final Job from = fromAlloc.getJob();
-        final Job to = toAlloc.getJob();
-        if (from.isSource() || to.isSink()) {
-            return;
-        }
-        if (this.hasRelation(from, to)) {
+        if (this.isBondBroken(from, to)) {
             return;
         }
         if (!PrecedenceRelationsTracker.isPrecedenceCorrect(fromAlloc, toAlloc)) {
-            this.totalCachedResult += PrecedenceRelationsTracker.relate(from, to, this.relations);
+            this.totalCachedResult += PrecedenceRelationsTracker.breakBond(from, to, this.brokenBonds);
         }
     }
 
-    private void formRelation(final Allocation from, final Job to) {
+    private void bind(final Allocation from, final Job to) {
         final Allocation a = this.allocations.get(to);
         if (a == null) {
             return;
         }
-        this.formRelation(from, a);
+        this.bind(from, a);
     }
 
-    private void formRelation(final Job from, final Allocation to) {
+    private void bind(final Job from, final Allocation to) {
         final Allocation a = this.allocations.get(from);
         if (a == null) {
             return;
         }
-        this.formRelation(a, to);
+        this.bind(a, to);
     }
 
     public int getBrokenPrecedenceRelationsMeasure() {
         return this.totalCachedResult;
     }
 
-    private boolean hasRelation(final Job from, final Job to) {
-        final Set<Job> related = this.relations.get(from);
+    private boolean isBondBroken(final Job from, final Job to) {
+        final Set<Job> related = this.brokenBonds.get(from);
         if (related == null) {
             return false;
         }
@@ -148,14 +122,41 @@ public class PrecedenceRelationsTracker {
     public void remove(final Allocation a) {
         final Job current = a.getJob();
         for (final Job j : current.getPredecessors()) {
-            this.breakRelation(j, a);
+            this.unbind(j, a);
         }
         for (final Job j : current.getSuccessors()) {
-            this.breakRelation(a, j);
+            this.unbind(a, j);
         }
         this.allocations.remove(current);
         if (a.getStartDate() < current.getParentProject().getReleaseDate()) {
-            this.totalCachedResult -= PrecedenceRelationsTracker.getBrokenRelationValidation(current);
+            this.totalCachedResult -= PrecedenceRelationsTracker.getBrokenPrecedenceValidation(current);
         }
+    }
+
+    private void unbind(final Allocation fromAlloc, final Allocation toAlloc) {
+        final Job from = fromAlloc.getJob();
+        final Job to = toAlloc.getJob();
+        if (from.isSource() || to.isSink()) {
+            return;
+        }
+        if (this.isBondBroken(from, to)) {
+            this.totalCachedResult -= PrecedenceRelationsTracker.mendBond(from, to, this.brokenBonds);
+        }
+    }
+
+    private void unbind(final Allocation from, final Job to) {
+        final Allocation a = this.allocations.get(to);
+        if (a == null) {
+            return;
+        }
+        this.unbind(from, a);
+    }
+
+    private void unbind(final Job from, final Allocation to) {
+        final Allocation a = this.allocations.get(from);
+        if (a == null) {
+            return;
+        }
+        this.unbind(a, to);
     }
 }
