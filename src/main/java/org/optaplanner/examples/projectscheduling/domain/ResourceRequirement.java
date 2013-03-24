2633a4aeff6529b0feae2550842ffd1c7d8f6bd2
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2013-03-24 11:06:13 +0100

    Make code style like rest of optaplanner-examples: field and getters in same order

--- src/main/java/org/optaplanner/examples/projectscheduling/domain/ResourceRequirement.java
+++ src/main/java/org/optaplanner/examples/projectscheduling/domain/ResourceRequirement.java
@@ -5,17 +5,17 @@ public class ResourceRequirement {
     private final Resource resource;
     private final int requirement;
 
-    public ResourceRequirement(final Resource r, final int requirement) {
-        this.resource = r;
+    public ResourceRequirement(final Resource resource, final int requirement) {
+        this.resource = resource;
         this.requirement = requirement;
-    }
-
-    public int getRequirement() {
-        return this.requirement;
     }
 
     public Resource getResource() {
         return this.resource;
+    }
+
+    public int getRequirement() {
+        return this.requirement;
     }
 
 }
