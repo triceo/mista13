74a8cdc5b218494b0eeb94b50f6a2649c3f8d2b2
Lukáš Petrovický <lpetrovi@redhat.com>
2013-03-24 10:55:23 +0100

    Make sure resource requirement is never < 1.

--- src/main/java/org/optaplanner/examples/projectscheduling/domain/ResourceRequirement.java
+++ src/main/java/org/optaplanner/examples/projectscheduling/domain/ResourceRequirement.java
@@ -6,6 +6,9 @@ public class ResourceRequirement {
     private final int requirement;
 
     public ResourceRequirement(final Resource resource, final int requirement) {
+        if (requirement < 1) {
+            throw new IllegalArgumentException("Zero requirements are useless, you needn't create those.");
+        }
         this.resource = resource;
         this.requirement = requirement;
     }
