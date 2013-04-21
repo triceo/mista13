097cb374f6404a5c4c594b9b6dc74d0bb94a5fff
Lukáš Petrovický <lpetrovi@redhat.com>
2013-04-21 09:31:40 +0200

    Upgrade to new API.

--- src/main/java/org/optaplanner/examples/projectscheduling/persistence/Mista2013DaoImpl.java
+++ src/main/java/org/optaplanner/examples/projectscheduling/persistence/Mista2013DaoImpl.java
@@ -1,9 +1,9 @@
 package org.optaplanner.examples.projectscheduling.persistence;
 
-import org.optaplanner.examples.common.persistence.XStreamSolutionDaoImpl;
+import org.optaplanner.examples.common.persistence.XStreamSolutionDao;
 import org.optaplanner.examples.projectscheduling.domain.ProjectSchedule;
 
-public class Mista2013DaoImpl extends XStreamSolutionDaoImpl {
+public class Mista2013DaoImpl extends XStreamSolutionDao {
 
     public Mista2013DaoImpl() {
         super("projectscheduling", ProjectSchedule.class);
