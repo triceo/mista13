e1c489b4be4294741ab68851370bd994adcc14b4
Lukáš Petrovický <lpetrovi@redhat.com>
2013-05-03 17:36:45 +0200

    Tango now static.

--- src/main/java/org/optaplanner/examples/projectscheduling/swingui/GanttPanel.java
+++ src/main/java/org/optaplanner/examples/projectscheduling/swingui/GanttPanel.java
@@ -4,14 +4,6 @@ import java.util.Date;
 
 import javax.swing.BoxLayout;
 
-import org.optaplanner.core.impl.solution.Solution;
-import org.optaplanner.examples.common.swingui.SolutionPanel;
-import org.optaplanner.examples.common.swingui.TangoColorFactory;
-import org.optaplanner.examples.projectscheduling.domain.Allocation;
-import org.optaplanner.examples.projectscheduling.domain.Job;
-import org.optaplanner.examples.projectscheduling.domain.ProjectSchedule;
-import org.optaplanner.examples.projectscheduling.domain.Project;
-import org.optaplanner.examples.projectscheduling.solver.solution.Mista2013SolutionCloner;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -22,15 +14,23 @@ import org.jfree.data.gantt.TaskSeries;
 import org.jfree.data.gantt.TaskSeriesCollection;
 import org.jfree.data.time.SimpleTimePeriod;
 import org.joda.time.DateTime;
+import org.optaplanner.core.impl.solution.Solution;
+import org.optaplanner.examples.common.swingui.SolutionPanel;
+import org.optaplanner.examples.common.swingui.TangoColorFactory;
+import org.optaplanner.examples.projectscheduling.domain.Allocation;
+import org.optaplanner.examples.projectscheduling.domain.Job;
+import org.optaplanner.examples.projectscheduling.domain.Project;
+import org.optaplanner.examples.projectscheduling.domain.ProjectSchedule;
+import org.optaplanner.examples.projectscheduling.solver.solution.Mista2013SolutionCloner;
 
 public class GanttPanel extends SolutionPanel {
 
     private static final long serialVersionUID = -1962577861486577643L;
-    
+
     private static final Mista2013SolutionCloner CLONER = new Mista2013SolutionCloner();
 
     public static IntervalCategoryDataset createDataset(final ProjectSchedule input) {
-        ProjectSchedule solution = GanttPanel.CLONER.cloneSolution(input);
+        final ProjectSchedule solution = GanttPanel.CLONER.cloneSolution(input);
         final TaskSeriesCollection collection = new TaskSeriesCollection();
         for (final Project p : solution.getProblem().getProjects()) {
             final TaskSeries series = new TaskSeries("Proj. " + p.getId());
@@ -57,14 +57,14 @@ public class GanttPanel extends SolutionPanel {
 
     public GanttPanel() {
     }
-    
-    private final TangoColorFactory tango = new TangoColorFactory();
-    
+
+    private static final TangoColorFactory TANGO = new TangoColorFactory();
+
     private JFreeChart createChart(final IntervalCategoryDataset dataset) {
-        JFreeChart c = ChartFactory.createGanttChart(null, null, null, dataset, true, false, false);
-        CategoryPlot plot = (CategoryPlot) c.getPlot();
+        final JFreeChart c = ChartFactory.createGanttChart(null, null, null, dataset, true, false, false);
+        final CategoryPlot plot = (CategoryPlot) c.getPlot();
         for (int i = 0; i < dataset.getRowCount(); i++) {
-            plot.getRenderer().setSeriesPaint(i, tango.pickColor(i));
+            plot.getRenderer().setSeriesPaint(i, GanttPanel.TANGO.pickColor(i));
         }
         return c;
     }
