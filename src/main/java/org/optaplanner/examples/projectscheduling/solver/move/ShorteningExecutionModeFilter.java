bb873954a0e9c58b45fc9b34a2035b601b66e60a
Lukáš Petrovický <lpetrovi@redhat.com>
2013-06-14 10:40:39 +0200

    Reproducing configuration.

---
+++ src/main/java/org/optaplanner/examples/projectscheduling/solver/move/ShorteningExecutionModeFilter.java
@@ -0,0 +1,25 @@
+package org.optaplanner.examples.projectscheduling.solver.move;
+
+import org.optaplanner.core.impl.heuristic.selector.common.decorator.SelectionFilter;
+import org.optaplanner.core.impl.heuristic.selector.move.generic.ChangeMove;
+import org.optaplanner.core.impl.score.director.ScoreDirector;
+import org.optaplanner.examples.projectscheduling.domain.Allocation;
+import org.optaplanner.examples.projectscheduling.domain.ExecutionMode;
+
+
+public class ShorteningExecutionModeFilter implements SelectionFilter<ChangeMove> {
+
+    @Override
+    public boolean accept(ScoreDirector scoreDirector, ChangeMove selection) {
+        ExecutionMode newValue = (ExecutionMode) selection.getToPlanningValue();
+        ExecutionMode currentValue = ((Allocation)selection.getEntity()).getExecutionMode();
+        if (newValue == currentValue) {
+            return false;
+        } else if (newValue.getDuration() > currentValue.getDuration()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
